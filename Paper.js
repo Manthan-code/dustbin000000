99330fe4be70662c11764b125ba0f76d08c6821c
Manthan-code <72066921+Manthan-code@users.noreply.github.com>
2020-11-25 16:01:36 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,26 @@
+class Paper{
+    constructor(){
+        var options ={
+            
+            restitution :0,
+            friction :0,
+            density :1.2
+            
+        }
+        this.body = Bodies.circle(250,540,20,options);
+        this.radius=20;
+       this.Paper=loadImage("paper.png");
+        World.add(world, this.body);
+    }
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(RADIUS);
+        image(this.Paper,0,0,this.radius,this.radius);
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground,ball;
+var binImg,bin;
+
+function preload(){
+
+    dustbin0=loadImage("dustbin.png");
+
+}
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    dustbin1=createSprite(964,505,10,10);
+    dustbin1.addImage(dustbin0);
+    dustbin1.scale=0.55;
+
+   
+    ground = new Ground();
+    crumpledPaper = new Paper();
+
+
+    binPart1 = new Dustbin(902,505,10,120);
+    binPart2 = new Dustbin(962,565,130,10);
+    binPart3 = new Dustbin(1024,505,10,120);
+}
+
+function draw(){
+    background("black");
+    Engine.update(engine);
+    
+    ground.display();
+    crumpledPaper.display();
+    binPart1.display();
+    binPart2.display();
+    binPart3.display(); 
+      
+    drawSprites();
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        Matter.Body.applyForce(crumpledPaper.body,crumpledPaper.body.position,{x:74,y:-75});
+
+    }
+}
